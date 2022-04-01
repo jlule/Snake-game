421a27ea23a6a0b2071f8a1043b2e29bd4e097a5
James Lule <43647523+jlule@users.noreply.github.com>
2022-03-31 18:24:27 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,38 @@
+// import logo from './logo.svg';
+// import Board from './Board/Board.jsx';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import Board from './Board/Board.jsx';
+
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <Board></Board>
+  </div>
+);
+
+export default App;
